b381b88ebb14f741837345f17ca06ab49ceddc86
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2023-03-22 18:07:45 -0300

    retirado os alerts e colocado para quando adicionar produto os campos de inputs ficarão em brancos

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ class Produto {
     }
 
     adicionar(){
-        alert('entrei no adicionar')
         let produto = this.lerDados();
         if(this.validar(produto)){
-            alert('entrei no if do adicionar')
             this.salvar(produto);
         }
 
         this.listar();
+        this.cancelar();
     }
 
     lerDados(){
-        alert('entrei no ler dados')
         let produto = {}
         produto.id = this.id;
         produto.nomeProduto = document.getElementById('pdnome').value;
@@ -26,7 +24,6 @@ class Produto {
     }
 
     validar(produto){
-        alert('entrei no validar')
         let msg = '';
 
         if (produto.nomeProduto == ''){
@@ -37,25 +34,21 @@ class Produto {
             msg += 'Por favor, insira corretamente o Valor do produto! \n';
         }
         if(msg != ''){
-            alert(msg);
             return false;
         }
         return true;
     }
 
     salvar(produto){
-        alert('entrei no salvar')
         this.arrayProdutos.push(produto);
         this.id++;
     }
 
     listar(){
-        alert('entrei no listar')
         let tbody = document.getElementById('tbody')
         tbody.innerText = '';
 
         for(let i = 0; i < this.arrayProdutos.length; i++){
-            alert('entrei no for do adicionar')
             let tr = tbody.insertRow();
             let td_id = tr.insertCell();
             let td_nome = tr.insertCell();
@@ -78,11 +71,11 @@ class Produto {
     }
 
     deletar(id){
-        let tbody = document.getElementById('tbody')
+        let tbody = document.getElementById('tbody');
         for (let i = 0 ; i < this.arrayProdutos.length; i++){
             if(this.arrayProdutos[i].id == id){
-                this.arrayProdutos.splice(i,1)
-                tbody.deleteRow(i)
+                this.arrayProdutos.splice(i,1);
+                tbody.deleteRow(i);
             }
         }
     }
